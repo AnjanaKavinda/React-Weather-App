fd74ad8c772b5d11a728fe8e0f3ff8dc237df65f
santh <sanath@liveroom.xyz>
2020-05-14 14:20:40 +0530

    Updates

---
+++ precache-manifest.5a6e94413c83f6d0069a985b54cba51e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7f846deab3262326fe48b3871e5243",
+    "url": "/React-Weather-App/index.html"
+  },
+  {
+    "revision": "a58e1c1e98b853493fbb",
+    "url": "/React-Weather-App/static/css/main.b26b8b88.chunk.css"
+  },
+  {
+    "revision": "3deec6dc0b43bb1d1c9e",
+    "url": "/React-Weather-App/static/js/2.329d0bb4.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/React-Weather-App/static/js/2.329d0bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58e1c1e98b853493fbb",
+    "url": "/React-Weather-App/static/js/main.f25cfdd7.chunk.js"
+  },
+  {
+    "revision": "4b4a8c98f54f1099ab95",
+    "url": "/React-Weather-App/static/js/runtime-main.d890c703.js"
+  }
+]);
