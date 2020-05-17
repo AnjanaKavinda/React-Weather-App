56c924e0f8500b4126f078d93f543fcabf1e6a07
anjana kavinda <anjanakavinda@gmail.com>
2020-05-17 15:29:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -144,7 +144,7 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
   const api = {
-    key: "e0b3d83f5334432ea6f5522dac4052da",
+    key: "xxxxxxxxxxxxxxxxxxxxxxx",
     base: "https://api.weatherbit.io/v2.0/"
 
   };
